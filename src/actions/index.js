1c3df0ffed28d974243bb6c749c7bcea325197aa
manis404 <manis404@gmail.com>
2018-06-03 17:27:47 +0200

    init

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+import { AUTH_USER, AUTH_ERROR } from "./types";
+
+export const signup = ({ email, password }, callback) => async dispatch => {
+  try {
+    const response = await axios.post("http://localhost:3090/signup", {
+      email: email,
+      password: password
+    });
+
+    dispatch({ type: AUTH_USER, payload: response.data.token });
+    callback();
+  } catch (e) {
+    dispatch({ type: AUTH_ERROR, payload: "Email in use" });
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+
+import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
+
+import reducers from "./reducers";
+import App from "./components/App";
+import Welcome from "./components/Welcome";
+import Signup from "./components/auth/Signup";
+
+import Feature from "./components/Feature";
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/feature" exact component={Feature} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector("#root")
+);
